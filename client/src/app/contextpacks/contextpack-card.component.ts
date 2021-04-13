06feadf3c5b9b452b4a6034c05ccf90106d034a7
Josh Quist <quist127@morris.umn.edu>
2021-04-13 14:29:21 -0500

    Final Admin fix
    This should finally fix the admin stuff not working for everyone.

--- client/src/app/contextpacks/contextpack-card.component.ts
+++ client/src/app/contextpacks/contextpack-card.component.ts
@@ -46,7 +46,7 @@ export class ContextPackCardComponent implements OnInit {
         Validators.required,
       ]))
     });
-    this.isAdmin = this.contextpackservice.checkIfAdmin(localStorage.getItem('loggedIn'));
+    this.isAdmin = this.contextpackservice.checkIfAdmin(localStorage.getItem('admin'));
   }
 
   displayEnabled(status: boolean){

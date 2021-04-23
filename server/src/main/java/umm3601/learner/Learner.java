d884be9626b7214e970434f2b7142411cd4966e5
Jeffrey Miller <Mill7707@umn.edu>
2021-04-23 16:46:56 -0500

    Change array value to ContextPack
    change array list value from string to ContextPack

--- server/src/main/java/umm3601/learner/Learner.java
+++ server/src/main/java/umm3601/learner/Learner.java
@@ -6,11 +6,13 @@ import javax.persistence.Id;
 
 import org.mongojack.ObjectId;
 
+import umm3601.contextpack.ContextPack;
+
 public class Learner {
 
   @Id @ObjectId
   public String _id;
 
   public String name;
-  public ArrayList<String> assignedContextPacks;
+  public ArrayList<ContextPack> assignedContextPacks;
 }

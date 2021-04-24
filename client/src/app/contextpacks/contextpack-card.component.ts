c883903a886373140ecc5d659d1893adf34a4a43
Jeffrey Miller <Mill7707@umn.edu>
2021-04-23 20:29:17 -0500

    Remove unneeded import

--- client/src/app/contextpacks/contextpack-card.component.ts
+++ client/src/app/contextpacks/contextpack-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, Router } from '@angular/router';
 import { ContextPack, Word, Wordlist} from './contextpack';
 import { ContextPackService } from './contextpack.service';
 

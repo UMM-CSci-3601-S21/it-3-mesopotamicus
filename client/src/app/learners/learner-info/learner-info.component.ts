5070b12fa4fc886dfad96bc2621fcfea1c4d2404
lloydhilsgen001 <2020hilsl93@moundsviewschools.org>
2021-04-25 20:20:02 -0500

    Added Learner Sub
    Imports learner from learnerSub

--- client/src/app/learners/learner-info/learner-info.component.ts
+++ client/src/app/learners/learner-info/learner-info.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MatSnackBar} from '@angular/material/snack-bar';
+import { Learner } from '../learner';
+import { LearnerService } from '../learner.service';
 
 @Component({
   selector: 'app-learner-info',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnerInfoComponent implements OnInit {
 
-  constructor() { }
+  public learnerName: string;
+
+  learner: Learner;
+  id: string;
+  getLearnerSub: Subscription;
+
+  constructor(private route: ActivatedRoute, private snackBar: MatSnackBar,
+    private learnerService: LearnerService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((pmap) => {
+      this.id = pmap.get('id');
+      if (this.getLearnerSub) {
+        this.getLearnerSub.unsubscribe();
+      }
+      this.getLearnerSub = this.learnerService.getLearnerById(this.id).subscribe(learner => this.learner = learner);
+    });
   }
 
+  onDestroy(): void {
+    if (this.getLearnerSub) {
+      this.getLearnerSub.unsubscribe();
+    }
+  }
 }

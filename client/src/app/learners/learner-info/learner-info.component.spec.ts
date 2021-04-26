83c35ee9d046ccfa90b2fa059543a65af4b087ef
lloydhilsgen001 <2020hilsl93@moundsviewschools.org>
2021-04-26 14:23:30 -0500

    Created test learner
    Setup learner-info testing to have a functional test learner and learner service

--- client/src/app/learners/learner-info/learner-info.component.spec.ts
+++ client/src/app/learners/learner-info/learner-info.component.spec.ts
@@ -1,21 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { ActivatedRouteStub } from '../../../testing/activated-route-stub';
+import { MockLearnerService } from '../../../testing/learner.service.mock';
 import { LearnerInfoComponent } from './learner-info.component';
+import { Learner } from '../learner';
+import { LearnerService } from '../learner.service';
 
 describe('LearnerInfoComponent', () => {
   let component: LearnerInfoComponent;
   let fixture: ComponentFixture<LearnerInfoComponent>;
+  let learnerService: LearnerService
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LearnerInfoComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MatCardModule,
+        MatSnackBarModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [LearnerInfoComponent],
+      providers: [
+        { provide: LearnerService, useValue: new MockLearnerService() },
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
-    .compileComponents();
-  });
+      .compileComponents().catch(error => {
+        expect(error).toBeNull();
+      });
+      learnerService = TestBed.inject(LearnerService);
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LearnerInfoComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    const testLearner: Learner =
+    {
+      _id: 'test_id',
+      name: 'test',
+      assignedContextPacks: ['contextpacks']
+    };
+
+    component.learner = testLearner;
+
     fixture.detectChanges();
   });
 
